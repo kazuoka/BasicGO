fe722128602b283d4ce0c40bc0306c50870c6417
Natchanon Uthitasan <kazuokanarita9@gmail.com>
2021-03-16 23:57:41 +0700

    Add file boolean
    boolean file

---
+++ boolean.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Boolean
+
+func main() {
+	a := true
+	b := false
+	fmt.Println(a)
+	fmt.Println(b)
+	// someBoolean := 5 < 3
+	// someBoolean := 5 > 3
+	// someBoolean := 5 == 3
+	// someBoolean := 5 >= 3
+	someBoolean := 5 <= 3
+	fmt.Println(someBoolean)
+}
